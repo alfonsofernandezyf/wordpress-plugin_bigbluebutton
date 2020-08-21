a07d94b471ea9cfa58ec7709c768ea6ba8d8c42a
Alfonso Fernández <alfonsofernandezyf@gmail.com>
2020-08-21 15:02:40 -0500

    Update bigbluebutton-public.js

--- public/js/bigbluebutton-public.js
+++ public/js/bigbluebutton-public.js
@@ -29,7 +29,7 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
-	$( window ).load( function() {
+	$(window).on("load", function (e) {
 
 		// display/hide recordings
 		$( '.bbb-recordings-display' ).click( function() {
